d7127f8f69a45d0d6996c2ba48839fc6fcf9f31e
fouad <>
2021-07-28 08:48:22 +0100

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -3,21 +3,22 @@
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
 
-sudo reflector -c Germany -a 12 --sort rate --save /etc/pacman.d/mirrorlist
+#sudo reflector -c Germany -a 12 --sort rate --save /etc/pacman.d/mirrorlist
+#sudo pacman -Syy
 
-sudo firewall-cmd --add-port=1025-65535/tcp --permanent
-sudo firewall-cmd --add-port=1025-65535/udp --permanent
-sudo firewall-cmd --reload
+#sudo firewall-cmd --add-port=1025-65535/tcp --permanent
+#sudo firewall-cmd --add-port=1025-65535/udp --permanent
+#sudo firewall-cmd --reload
 
-git clone https://aur.archlinux.org/pikaur.git
-cd pikaur/
-makepkg -si --noconfirm
+#git clone https://aur.archlinux.org/paru.git
+#cd paru
+#makepkg -si --noconfirm
 
-#pikaur -S --noconfirm system76-power
+#paru -S --noconfirm system76-power
 #sudo systemctl enable --now system76-power
 #sudo system76-power graphics integrated
-#pikaur -S --noconfirm gnome-shell-extension-system76-power-git
-#pikaur -S --noconfirm auto-cpufreq
+#paru -S --noconfirm gnome-shell-extension-system76-power-git
+#paru -S --noconfirm auto-cpufreq
 #sudo systemctl enable --now auto-cpufreq
 
 sudo pacman -S --noconfirm xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4 xfce4-goodies firefox  arc-gtk-theme arc-icon-theme  vlc
