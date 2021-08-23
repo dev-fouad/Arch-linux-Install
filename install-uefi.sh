dad376924946397e234cca5e681bee169c6c1ae8
fouad-d <fouad@arch-fouad.localdomain>
2021-08-23 01:06:19 +0100

    update

--- install-uefi.sh
+++ install-uefi.sh
@@ -10,15 +10,14 @@ echo "arch-fouad" >> /etc/hostname
 echo "127.0.0.1 	localhost" >> /etc/hosts
 echo "::1       	localhost" >> /etc/hosts
 echo "127.0.1.1 	arch-fouad.localdomain		arch-fouad" >> /etc/hosts
-echo root:password | chpasswd
+#echo root:password | chpasswd
 
 # You can add xorg to the installation packages, I usually add it at the DE or WM install script
 # You can remove the tlp package if you are installing on a desktop or vm
 
-pacman -S grub efibootmgr networkmanager network-manager-applet dialog wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pipewire pipewire-alsa pipewire-pulse pipewire-jack bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra edk2-ovmf bridge-utils dnsmasq vde2 openbsd-netcat iptables-nft ipset firewalld flatpak sof-firmware nss-mdns acpid os-prober ntfs-3g terminus-font
+pacman -S --noconfirm grub efibootmgr networkmanager network-manager-applet wireless_tools wpa_supplicant xf86-video-intel xorg xorg-xwininfo xorg-xinit xorg-xprop xorg-xdpyinfo xorg-xbacklight pulseaudio-bluetooth pulseaudio-alsa pulsemixer pamixer
 
-# pacman -S --noconfirm xf86-video-amdgpu
-# pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+pacman -S --noconfirm pacman-contrib dialog mtools dosfstools base-devel linux-headers xdg-user-dirs xdg-utils gvfs bluez bluez-utils cups bash-completion openssh git polkit polkit-gnome gnome-keyring 
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
@@ -27,22 +26,22 @@ systemctl enable NetworkManager
 systemctl enable bluetooth
 systemctl enable cups.service
 systemctl enable sshd
-systemctl enable avahi-daemon
-systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
-systemctl enable reflector.timer
-systemctl enable fstrim.timer
-systemctl enable libvirtd
-systemctl enable firewalld
-systemctl enable acpid
+#systemctl enable avahi-daemon
+#systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
+#systemctl enable reflector.timer
+#systemctl enable fstrim.timer
+#systemctl enable libvirtd
+#systemctl enable firewalld
+#systemctl enable acpid
 
 useradd -mG fouad
-echo fouad:password | chpasswd
-usermod -aG libvirt ermanno
+#echo fouad:password | chpasswd
+#usermod -aG libvirt fouad
 
-echo "fouad ALL=(ALL) ALL" >> /etc/sudoers.d/ermanno
+#echo "fouad ALL=(ALL) ALL" >> /etc/sudoers.d/fouad
 
 
-printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
+#printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
 
 
 
