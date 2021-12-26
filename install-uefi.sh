87a73ae391d46f1abfebbc683661e39c2b0d722e
fouad <29574820+fouad-d@users.noreply.github.com>
2021-12-26 22:01:16 +0100

    Update install-uefi.sh

--- install-uefi.sh
+++ install-uefi.sh
@@ -25,11 +25,11 @@ systemctl enable bluetooth
 systemctl enable cups.service
 systemctl enable sshd
 #systemctl enable avahi-daemon
-#systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
+systemctl enable tlp # You can comment this command out if you didn't install tlp, see above
 #systemctl enable reflector.timer
 #systemctl enable fstrim.timer
 #systemctl enable firewalld
-#systemctl enable acpid
+systemctl enable acpid
 
 useradd -mG fouad
 #echo fouad:password | chpasswd
